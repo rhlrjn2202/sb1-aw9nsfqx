887553bd41e3375eaf6ac083fd13e1e0b37364c9
rhlrjn2202 <46061702+rhlrjn2202@users.noreply.github.com>
2024-12-23 19:05:39 +0530

    1

--- README.md
+++
@@ -1,3 +0,0 @@
-# sb1-aw9nsfqx
-
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/rhlrjn2202/sb1-aw9nsfqx)
--- lib/utils/connectivity_service.dart
+++
@@ -1,8 +0,0 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
-
-class ConnectivityService {
-  static Future<bool> hasInternetConnection() async {
-    final connectivityResult = await Connectivity().checkConnectivity();
-    return connectivityResult != ConnectivityResult.none;
-  }
-}
